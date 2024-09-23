0312b63e599b5c3e72510ac557a3a100b54f6e08
BananfonBan <bananfonban@yandex.ru>
2024-09-23 17:23:21 +0300

    Test

--- database.sql
+++ database.sql
@@ -14,166 +14,161 @@ quizzes,
 exercises,
 discussions,
 blogs CASCADE;
-
---Создание Перечисляемых типов (ENUM)
-CREATE TYPE users_role AS ENUM ('Student', 'teacher', 'Admin');
-
-CREATE TYPE enrollments_status AS ENUM ('active', 'pending', 'cancelled', 'completed');
-
-CREATE TYPE payments_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
-
-CREATE TYPE programs_completions AS ENUM ('active', 'completed', 'pending', 'cancelled');
-
-CREATE TYPE articles_status AS ENUM ('created', 'in moderation', 'published', 'archived');
-
--- Step 1
-CREATE TABLE programs (
+CREATE TABLE courses(
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255),
-    price NUMERIC,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+CREATE TABLE lessons(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    course_id BIGINT REFERENCES courses (id) ON DELETE
+    SET NULL,
+        title VARCHAR(255) NOT NULL,
+        content TEXT,
+        video_url VARCHAR(255),
+        position SMALLINT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        deleted_at TIMESTAMP
+);
+CREATE TABLE modules(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+CREATE TABLE programs(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(10, 2),
     program_type VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE modules (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR (255),
-    description VARCHAR (255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at BOOLEAN DEFAULT FALSE
-);
-
-CREATE TABLE courses (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255),
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at BOOLEAN DEFAULT FALSE
-);
-
-CREATE TABLE lessons (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    course_id BIGINT REFERENCES courses(id) ON DELETE SET NULL,
-    name VARCHAR(255),
-    content TEXT,
-    video_url VARCHAR(255),
-    position INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at BOOLEAN DEFAULT FALSE
-);
-
--- Связи таблиц
 CREATE TABLE course_modules (
     course_id BIGINT REFERENCES courses(id) ON DELETE CASCADE,
-    module_id BIGINT REFERENCES modules(id) ON DELETE CASCADE
-    PRIMARY KEY (course_id, module_id)
+    module_id BIGINT REFERENCES modules(id) ON DELETE CASCADE,
+    PRIMARY KEY (module_id, course_id)
 );
-
 CREATE TABLE program_modules (
     program_id BIGINT REFERENCES programs(id) ON DELETE CASCADE,
     module_id BIGINT REFERENCES modules(id) ON DELETE CASCADE,
     PRIMARY KEY (program_id, module_id)
 );
-
--- Step 2
-CREATE TABLE teaching_groups (
+CREATE TABLE teaching_groups(
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    slug VARCHAR(255),
+    slug VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE users (
+CREATE TABLE users(
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    password_hash TEXT,
-    role users_role NOT NULL,
-    teaching_group_id BIGINT REFERENCES teaching_groups(id) ON DELETE SET NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at BOOLEAN DEFAULT FALSE
-);
-
--- Step 3
-CREATE TABLE enrollments (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
-    program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
-    status enrollments_status,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE payments (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    enrollment_id BIGINT REFERENCES enrollments(id) ON DELETE SET NULL,
-    amount NUMERIC,
-    status payments_status NOT NULL,
-    paid_at TIMESTAMP,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE program_completions (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
-    program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
-    status programs_completions NOT NULL,
-    started_at DATE,
-    completed_at DATE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE certificates (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
-    program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
-    url VARCHAR(255),
-    issued_at DATE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Step 4
-CREATE TABLE quizzes (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
-    name VARCHAR(255),
-    content JSONB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE exercises (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
-    name VARCHAR(255),
-    url VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE discussions (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
-    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
-    text JSONB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE blogs (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
     name VARCHAR(255) NOT NULL,
-    content TEXT,
-    status articles_status NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255),
+    role VARCHAR(10) CHECK (role IN ('Student', 'Teacher', 'Admin')) NOT NULL,
+    teaching_group_id BIGINT REFERENCES teaching_groups(id) ON DELETE
+    SET NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        deleted_at TIMESTAMP
+);
+CREATE TABLE enrollments(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES users(id) ON DELETE
+    SET NULL,
+        program_id BIGINT REFERENCES programs(id) ON DELETE
+    SET NULL,
+        status VARCHAR(20) CHECK (
+            status IN ('active', 'pending', 'cancelled', 'completed')
+        ) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE payments(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    enrollment_id BIGINT REFERENCES enrollments(id) ON DELETE
+    SET NULL,
+        amount NUMERIC(10, 2),
+        status VARCHAR(20) CHECK (
+            status IN ('pending', 'paid', 'failed', 'refunded')
+        ) NOT NULL,
+        paid_at DATE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE program_completions(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES users(id) ON DELETE
+    SET NULL,
+        program_id BIGINT REFERENCES programs(id) ON DELETE
+    SET NULL,
+        status VARCHAR(20) CHECK (
+            status IN ('active', 'completed', 'pending', 'cancelled')
+        ) NOT NULL,
+        started_at DATE,
+        completed_at DATE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE certificates(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES users(id) ON DELETE
+    SET NULL,
+        program_id BIGINT REFERENCES programs(id) ON DELETE
+    SET NULL,
+        url VARCHAR(255),
+        issued_at DATE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE quizzes(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    lesson_id BIGINT REFERENCES lessons (id) ON DELETE
+    SET NULL,
+        title VARCHAR(255),
+        content JSONB,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE exercises(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    lesson_id BIGINT REFERENCES lessons (id) ON DELETE
+    SET NULL,
+        name VARCHAR(255),
+        url VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE discussions(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    lesson_id BIGINT REFERENCES lessons (id) ON DELETE
+    SET NULL,
+        user_id BIGINT REFERENCES users (id) ON DELETE
+    SET NULL,
+        text JSONB,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE blogs(
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES users (id) ON DELETE
+    SET NULL,
+        title VARCHAR(255) NOT NULL,
+        content TEXT,
+        status VARCHAR(20) CHECK (
+            status IN (
+                'created',
+                'in moderation',
+                'published',
+                'archived'
+            )
+        ) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
