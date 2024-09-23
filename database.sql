878cc2e8cd3d3da67c965e7d6137df2e450c6789
BananfonBan <bananfonban@yandex.ru>
2024-09-23 11:54:19 +0300

    Step 3

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+-- Step 1
 CREATE TABLE Programs (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title VARCHAR(50),
@@ -37,7 +38,6 @@ CREATE TABLE Lessons (
 );
 
 -- Связи табли
-
 CREATE TABLE program_modules (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     program_id BIGINT REFERENCES Programs(id),
@@ -56,6 +56,9 @@ CREATE TABLE course_lessons (
     lesson_id BIGINT REFERENCES Lessons(id)
 );
 
+-- Пользователи и Группы (Step 2)
+CREATE TYPE users_role AS ENUM ('student', 'teacher', 'admin');
+
 CREATE TABLE TeachingGroups (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     slug VARCHAR (255),
@@ -69,7 +72,53 @@ CREATE TABLE Users (
     email VARCHAR(255),
     password TEXT,
     teachinggroup_id BIGINT REFERENCES TeachingGroups(id),
-    role VARCHAR(50),
+    role users_role,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+-- Step 3
+CREATE TYPE enrollments_status AS ENUM ('active', 'pending', 'cancelled', 'completed');
+
+CREATE TYPE payments_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
+
+CREATE TYPE programs_completions AS ENUM ('active', 'completed', 'pending', 'cancelled');
+
+CREATE TABLE Enrollments (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES Users(id),
+    program_id BIGINT REFERENCES Programs(id),
+    enrollment_status enrollments_status,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE Payments (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    enrollment_id BIGINT REFERENCES Enrollments(id),
+    payment_amount payments_status,
+    payment_date TIMESTAMP,
+    created_at TIMESTAMP,
+    update_at TIMESTAMP
+);
+
+CREATE TABLE ProgramCompletions (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES Uers(id),
+    program_id BIGINT REFERENCES Programs(id),
+    completion_status programs_completions,
+    start_program_date TIMESTAMP,
+    finish_program_date TIMESTAMP,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE Certificates (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES Users(id),
+    program_id BIGINT REFERENCES Programs(id),
+    url VARCHAR(50),
+    certificate_date TIMESTAMP,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
